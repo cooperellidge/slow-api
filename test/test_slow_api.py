471438e8ab56f02c511af70c9e4bcedea63653ab
Cooper Ellidge <cooper@ioenergy.com.au>
2024-05-29 09:44:50 +0930

    add coverage to tests script

--- test/test_slow_api.py
+++ test/test_slow_api.py
@@ -1,23 +1,32 @@
 """_summary_."""
 
+import pytest
 from slow_api import SlowAPI
 
 
-def test_add_index_route_with_decorator() -> None:
-    """_summary_."""
+@pytest.fixture(scope="function")
+def decorated_app() -> SlowAPI:
+    """_summary_.
+
+    Returns:
+        SlowAPI: _description_
+    """
     app = SlowAPI()
 
     @app.get("/")
     def get_index() -> bytes:
         return b"Hello World"
 
-    assert "/" in app.routes
-    assert "GET" in app.routes["/"]
-    assert app.routes["/"]["GET"] == get_index
+    return app
 
 
-def test_add_index_route_with_method() -> None:
-    """_summary_."""
+@pytest.fixture(scope="function")
+def app() -> SlowAPI:
+    """_summary_.
+
+    Returns:
+        SlowAPI: _description_
+    """
     app = SlowAPI()
 
     def get_index() -> bytes:
@@ -25,6 +34,18 @@ def test_add_index_route_with_method() -> None:
 
     app.add_route(get_index, "/", "GET")
 
+    return app
+
+
+def test_index_route_with_decorator(decorated_app: SlowAPI) -> None:
+    """_summary_."""
+    assert "/" in decorated_app.routes
+    assert "GET" in decorated_app.routes["/"]
+    assert decorated_app.routes["/"]["GET"]() == b"Hello World"
+
+
+def test_index_route_with_method(app: SlowAPI) -> None:
+    """_summary_."""
     assert "/" in app.routes
     assert "GET" in app.routes["/"]
-    assert app.routes["/"]["GET"] == get_index
+    assert app.routes["/"]["GET"]() == b"Hello World"
